d6251844e2b4b7f9289acbad05180f222af90b37
Filip <f.golub321@gmail.com>
2021-07-28 12:56:01 +0200

    Add player input, create function playRound

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,22 @@ function computerPlay(){
 }
 
 console.log(computerPlay());
+
+let input=prompt("Rock, paper or scissors?").toLowerCase();
+
+function playRound(playerSelection, computerSelection){
+    switch(playerSelection){
+        case "rock":
+            switch(computerSelection){
+                case "rock":
+                    return "Draw. Rock and rock."
+                    break;
+                case "paper":
+                    return "You lose! Paper beats rock."
+                    break;
+                case "scissors":
+                    return "You win! Rock beats scissors."
+                    break;
+            }
+    }
+}
