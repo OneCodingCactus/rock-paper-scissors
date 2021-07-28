cbba0a723d956a87b5981e3f239ac73d26563418
Filip <f.golub321@gmail.com>
2021-07-28 19:09:32 +0200

    Modified function playRound

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,28 @@
-function computerPlay(){
-    let options=[
+function computerPlay() {
+    let options = [
         "Rock",
         "Paper",
         "Scissors"
     ]
-    return options[Math.floor(Math.random()*options.length)];
+    return options[Math.floor(Math.random() * options.length)];
 }
 
 console.log(computerPlay());
 
-let input=prompt("Rock, paper or scissors?").toLowerCase();
+let input = prompt("Rock, paper or scissors?").toLowerCase();
 
-function playRound(playerSelection, computerSelection){
-    switch(playerSelection){
-        case "rock":
-            switch(computerSelection){
-                case "rock":
-                    return "Draw. Rock and rock."
-                    break;
-                case "paper":
-                    return "You lose! Paper beats rock."
-                    break;
-                case "scissors":
-                    return "You win! Rock beats scissors."
-                    break;
-            }
-    }
+function playRound(playerSelection, computerSelection) {
+    switch (true) {
+        case (playerSelection === "rock" && computerSelection === "paper"):
+        case (playerSelection === "paper" && computerSelection === "scissors"):
+        case (playerSelection === "scissors" && computerSelection === "rock"):
+            return "You lose! ${computerSelection} beats ${playerSelection}."
+            break;
+        case (playerSelection === computerSelection):
+            return "Draw. ${computerSelection} cannot ${playerSelection}."
+            break;
+        default:
+            return "You win! ${playerSelection} beats ${computerSelection}."
+            break;
+        }
 }
